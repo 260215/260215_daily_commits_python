af233c4f0646821a5d6238bcc3262a8a09cd22cb
260215_daily_commits_python <roysuvam1999@gmail.com>
2021-04-17 20:50:22 +0530

    my 12th commit on pycharm windows environment

---
+++ code 12.py
@@ -0,0 +1,12 @@
+# Relational and Logical Operators
+a, b, c = 2, 3, 4
+if a ** 2 == c:
+    print('a =', a)
+else:
+    print('b =', b)
+
+a, b, c = 5, 6, 7
+if a < c and a < c:
+    print(a)
+if not a == 5 or b < c:
+    print(c)
