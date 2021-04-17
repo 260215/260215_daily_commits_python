bbf12f1fdfeb0a69f36b1ec4ac852a5e601fb478
260215_daily_commits_python <roysuvam1999@gmail.com>
2021-04-17 23:15:56 +0530

    my 15th commit on pycharm in windows environment

---
+++ code 15.py
@@ -0,0 +1,15 @@
+# while loop
+i = 1
+while i <= 10:
+    print(i, end=' ')
+    i += 1
+
+print("\n")
+
+j = 1
+n = int(input("Enter the limit : "))
+sum = 0
+while j < n:
+    sum += i
+    j += 1
+print(sum)
