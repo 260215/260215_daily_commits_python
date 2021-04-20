bd9bf5566ea7443da0b0c6cbc1a9f84f2c235756
260215_daily_commits_python <roysuvam1999@gmail.com>
2021-04-21 02:00:02 +0530

    my 32nd commit on pycharm windows environment

---
+++ code 32.py
@@ -0,0 +1,21 @@
+'''"Hello Python"
+print(__doc__)'''
+
+'''a, b = [int(i) for i in input("Enter two numbers : ").split(' ')]
+res = a + b
+print(res)
+print(type(res))
+print(type(a), type(b))'''
+
+'''x, y = 10, 2.3
+s1, ch = "hello", 'A'
+flag = True
+c1 = 2 + 3j
+print(type(c1))
+print(isinstance(int))'''
+
+print(int(0x10))
+print(hex(45))
+print(oct(0o23))
+print(bin(13))
+print(int(0b11011))
